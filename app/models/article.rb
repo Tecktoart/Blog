74bd6ffd072e034bf948c645a21ffcd6b7a80029
Anton Jyha <jyha@1pt.com>
2017-10-24 11:56:02 +0300

    refactor validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,10 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true,
-                    length: { minimum: 5 }
-
   has_many :likes, class_name: 'Vote', through: :likes
   has_many :dislikes, class_name: 'Vote', through: :dislikes
-
   has_many :comments
 
   belongs_to :user
+
   validates :title, presence: true, length: { minimum: 5 }
-  validates_presence_of :user
+  validates :user, presence: true
 end
